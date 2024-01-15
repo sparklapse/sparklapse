bc4c48ce1ae07675c55d62f5f2c0c33c5409458a
mr-josh <mr-josh@sparklapse.com>
2024-01-15 11:21:47 +1100

    Initial setup
    Setup initial astro template to just show nice website with logo/title, and background.

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+import typeographyPlugin from "@tailwindcss/typography";
+import { dotsUIPlugin } from "@sparklapse/dots/ui/tailwind";
+
+/** @type {import("tailwindcss").Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [typeographyPlugin(), dotsUIPlugin()],
+};
