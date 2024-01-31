6dd35b3ff6dba1a24371e90ac5143d4fcdaa735d
mr-josh <mr-josh@sparklapse.com>
2024-01-31 13:42:14 +1100

    Added vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sparklapse.com",
   integrations: [mdx(), sitemap(), tailwind()],
+  output: "hybrid",
+  adapter: vercel({
+    webAnalytics: true,
+  }),
 });
