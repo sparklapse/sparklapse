8542368891012079c0ad9901183a5e155f35fd28
mr-josh <mr-josh@sparklapse.com>
2024-02-09 20:22:17 +1100

    Seeing if middleware location matters

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware(async ({ locals }, next) => {
-  const response = await next();
-  console.log(await response.text());
-  return await next();
-});
