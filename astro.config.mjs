3aeaa9d51a85c36e1fc0efc1952356e3bfb147a8
mr-josh <mr-josh@sparklapse.com>
2024-02-09 21:41:37 +1100

    Needed to use static adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -17,7 +17,7 @@ export default defineConfig({
       },
     },
   },
-  output: "hybrid",
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
