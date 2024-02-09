70c163b4a91c00d9060f9a3018c4afafec731017
mr-josh <mr-josh@sparklapse.com>
2024-02-09 19:54:37 +1100

    Disabled the geo-tz lookup for now

--- src/vercel-edge-middleware.ts
+++ src/vercel-edge-middleware.ts
@@ -1,5 +1,5 @@
 import { geolocation } from "@vercel/edge";
-import { find } from "geo-tz";
+// import { find } from "geo-tz";
 import type { RequestContext } from "@vercel/edge";
 
 export default function ({
@@ -12,10 +12,10 @@ export default function ({
   const loc = geolocation(request);
 
   let timezone = "Australia/Sydney";
-  if (loc.latitude && loc.longitude) {
-    let search = find(parseFloat(loc.latitude), parseFloat(loc.longitude));
-    if (search.length > 0) timezone = search[0];
-  }
+  // if (loc.latitude && loc.longitude) {
+  //   let search = find(parseFloat(loc.latitude), parseFloat(loc.longitude));
+  //   if (search.length > 0) timezone = search[0];
+  // }
 
   return { timezone };
 }
