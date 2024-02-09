9c8d673c018cf57a40a816d7a50c81d0b537d230
mr-josh <mr-josh@sparklapse.com>
2024-02-09 20:08:29 +1100

    More edge pain

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async ({ locals }, next) => {
+  const response = await next();
+  console.log(await response.text());
   return await next();
 });
--- src/vercel-edge-middleware.ts
+++ src/vercel-edge-middleware.ts
@@ -1,5 +1,4 @@
 import { geolocation } from "@vercel/edge";
-// import { find } from "geo-tz";
 import type { RequestContext } from "@vercel/edge";
 
 export default function ({
@@ -12,10 +11,6 @@ export default function ({
   const loc = geolocation(request);
 
   let timezone = "Australia/Sydney";
-  // if (loc.latitude && loc.longitude) {
-  //   let search = find(parseFloat(loc.latitude), parseFloat(loc.longitude));
-  //   if (search.length > 0) timezone = search[0];
-  // }
 
   return { timezone };
 }
