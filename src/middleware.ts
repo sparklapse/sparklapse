944baaef127163b2053dbd89e031d99e95a1a66f
mr-josh <mr-josh@sparklapse.com>
2024-02-09 20:53:24 +1100

    Made sure to set locals before rendering next

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async ({ locals, request }, next) => {
-  console.log("Hello from middleware!");
-
-  const response = await next();
-  console.log(request, response);
   locals.timezone = "Australia/Sydney";
+  const response = await next();
 
+  console.log(request, response);
   return response;
 });
