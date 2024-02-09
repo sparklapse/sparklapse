bf40881fa1d1a9e0f8f2fdc35cd1fe0bcf601af6
mr-josh <mr-josh@sparklapse.com>
2024-02-09 19:08:03 +1100

    Updated timezone edge middleware

--- src/vercel-edge-middleware.ts
+++ src/vercel-edge-middleware.ts
@@ -1,4 +1,5 @@
 import { geolocation } from "@vercel/edge";
+import { find } from "geo-tz";
 import type { RequestContext } from "@vercel/edge";
 
 export default function ({
@@ -10,5 +11,11 @@ export default function ({
 }) {
   const loc = geolocation(request);
 
-  return { timezone: "Australia/Sydney" };
+  let timezone = "EST";
+  if (loc.latitude && loc.longitude) {
+    let search = find(parseFloat(loc.latitude), parseFloat(loc.longitude));
+    if (search.length > 0) timezone = search[0];
+  }
+
+  return { timezone };
 }
