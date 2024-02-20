71a70c50e53955ccb29f5c6d236af19aa7099750
mr-josh <mr-josh@sparklapse.com>
2024-02-20 19:07:14 +1100

    Updated ability for user to update own password and name

--- content/schema.ts
+++ content/schema.ts
@@ -1,9 +1,8 @@
 import { User } from "./schemas/user";
 import { Event } from "./schemas/event";
 import { EventDoc } from "./schemas/event-doc";
-import type { Lists } from ".keystone/types";
 
-export const lists: Lists = {
+export const lists = {
   User,
   Event,
   EventDoc,
--- content/schemas/user.ts
+++ content/schemas/user.ts
@@ -1,28 +1,61 @@
 import { list } from "@keystone-6/core";
-import { allowAll } from "@keystone-6/core/access";
+import { allowAll, denyAll } from "@keystone-6/core/access";
 import { text, password, timestamp, checkbox } from "@keystone-6/core/fields";
-import type { Lists } from ".keystone/types";
 
-export const User: NonNullable<Lists["User"]> = list({
+export const User = list({
   access: {
     operation: {
       query: allowAll,
       create: ({ session }) => session?.data.isAdmin,
-      update: ({ session }) => session?.data.isAdmin,
       delete: ({ session }) => session?.data.isAdmin,
+      update: allowAll,
+    },
+    item: {
+      update: ({ session, item }) => {
+        if (session?.data.isAdmin) return true;
+        if (session?.data.id === item.id) return true;
+
+        return false;
+      },
     },
   },
   fields: {
-    name: text({ validation: { isRequired: true } }),
+    name: text({
+      access: {
+        update: ({ session, item }) => {
+          if (session?.data.isAdmin) return true;
+          if (session?.data.id === item.id) return true;
+
+          return false;
+        },
+      },
+      validation: { isRequired: true },
+    }),
     username: text({
+      access: {
+        update: ({ session, item }) => {
+          if (session?.data.isAdmin) return true;
+          if (session?.data.id === item.id) return true;
+
+          return false;
+        },
+      },
       validation: { isRequired: true },
       isIndexed: "unique",
     }),
     password: password({ validation: { isRequired: true } }),
     isAdmin: checkbox({
+      access: {
+        update: ({ session }) => session?.data.isAdmin,
+      },
       defaultValue: false,
     }),
     createdAt: timestamp({
+      access: {
+        create: ({ session }) => session?.data.isAdmin,
+        read: allowAll,
+        update: denyAll,
+      },
       defaultValue: { kind: "now" },
       ui: {
         itemView: {
