c8a3b9ba6d90de53179f496c54d8109829df53a8
mr-josh <mr-josh@sparklapse.com>
2024-02-22 19:29:30 +1100

    Removed astro-min potentially causing build errors

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,13 +3,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/static";
-import min from "astro-min";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sparklapse.com",
-  integrations: [mdx(), sitemap(), tailwind(), svelte(), min()],
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
   vite: {
     resolve: {
       alias: {
