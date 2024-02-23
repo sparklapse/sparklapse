78acbe1607c6dccef3f4750b807cad96785d4541
mr-josh <mr-josh@sparklapse.com>
2024-02-23 23:05:12 +1100

    Setup i18next for events and added translations for tetrio SSL 3

--- site/src/content/events/2024-04-06-ssl-3/_sections/Event.svelte
+++ site/src/content/events/2024-04-06-ssl-3/_sections/Event.svelte
@@ -6,51 +6,26 @@
 <script lang="ts">
   import Accordion from "$components/Accordion.svelte";
 
-  export let dates: Record<string, Date>;
+  export let items: {
+    id: string;
+    title: string;
+    aside?: Date;
+    description: string;
+  }[];
+  $: localised = items.map((item) => ({
+    ...item,
+    aside: item.aside?.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "2-digit",
+      timeZoneName: "shortGeneric",
+    }),
+  }));
+  export let initial: string;
 </script>
 
 <Accordion
-  items={[
-    {
-      id: "warm-up",
-      title: "🔥 Warm Up Games",
-      aside: dates.warmup.toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "2-digit",
-        timeZoneName: "shortGeneric",
-      }),
-      description: "Join early for rank-capped warm up games!\n(No prizes, just for fun!)",
-    },
-    {
-      id: "main-event",
-      title: "🏆 Main Event",
-      aside: dates.main.toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "2-digit",
-        timeZoneName: "shortGeneric",
-      }),
-      description:
-        "The main event consists of 8 standard games and 8 gimmick games, with each game lasting 3-8 minutes. Gimmick games are a staple of Super Streamer Lobbies, in which we run several custom game modes to maximize laughs and clippable moments.",
-    },
-    {
-      id: "afterparty",
-      title: "🎉 Afterparty Showcase",
-      aside: dates.afterparty.toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "2-digit",
-        timeZoneName: "shortGeneric",
-      }),
-      description:
-        "Come watch the Twitch showcase of the best clips and highlights from all 200 streamers in the event, along with some interviews and giveaways!",
-    },
-    {
-      id: "prizes",
-      title: "🎁 Prizes",
-      description:
-        "During the main event, have your chance at winning gifted subs to those streaming the event! A raffle will be open at the start of the main event for a chance to win fully paid comissioned work from @milkthos_sena. Winners of the raffle will be announced during the afterparty showcase.",
-    },
-  ]}
-  defaultValue="main-event"
+  items={localised}
+  defaultValue={initial}
   styles={{
     container: "w-full",
     item: "text-xl font-extrabold",
