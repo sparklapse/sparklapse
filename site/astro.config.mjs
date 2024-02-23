d4e92f3151ff2a09d2cafa492ac0b48108a5973b
mr-josh <mr-josh@sparklapse.com>
2024-02-24 00:32:58 +1100

    Events now redirect to available prefered language

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,7 +2,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 import i18next from "i18next";
@@ -14,6 +14,10 @@ await i18next.init({
 // https://astro.build/config
 export default defineConfig({
   site: "https://sparklapse.com",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ru", "ko"],
+  },
   integrations: [mdx(), sitemap(), tailwind(), svelte()],
   vite: {
     resolve: {
@@ -24,7 +28,7 @@ export default defineConfig({
       },
     },
   },
-  output: "static",
+  output: "hybrid",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
