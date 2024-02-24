a59e9833653e67d6d9ad0245e5fe5baab4362dc8
mr-josh <mr-josh@sparklapse.com>
2024-02-24 12:12:21 +1100

    Added japanese and spanish translations

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
   site: "https://sparklapse.com",
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "ru", "ko"],
+    locales: ["en", "ru", "ko", "ja", "es"],
   },
   integrations: [mdx(), sitemap(), tailwind(), svelte()],
   vite: {
